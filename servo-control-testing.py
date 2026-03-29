fca740441edec0fea2d20f9752e36105b18cd591
Noah R. <noahemmanuelramirez@gmail.com>
2026-03-29 16:59:39 -0400

    updated servo-control-testing.py
    improvements!!! the last variation works but is very slow.

--- servo-control-testing.py
+++ servo-control-testing.py
@@ -91,19 +91,19 @@ def key_pressed(key):
       kit.continuous_servo[wheel_loc].throttle=-1
     elif key == "e":
       if knee_pos <= knee_max:
-        knee_pos += 1
+        knee_pos += 5
         kit.servo[knee_loc].angle = knee_pos
     elif key == "q":
       if knee_pos >= knee_min:
-        knee_pos -= 1
+        knee_pos -= 5
         kit.servo[knee_loc].angle = knee_pos
     elif key == "d":
       if hip_pos <= hip_max:
-        hip_pos += 1
+        hip_pos += 5
         kit.servo[hip_loc].angle = hip_pos
     elif key == "a":
       if hip_pos >= hip_min:
-        hip_pos -= 1
+        hip_pos -= 5
         kit.servo[hip_loc].angle = hip_pos
   except AttributeError:
     if key == "esc":
