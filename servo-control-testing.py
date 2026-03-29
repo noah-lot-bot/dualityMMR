8e1908b8b69834048e7d2ede232b88e73e96abe9
Noah R. <noahemmanuelramirez@gmail.com>
2026-03-29 17:07:01 -0400

    updated servo-control-testing.py
    bug fixing, might need to revert to previous version

--- servo-control-testing.py
+++ servo-control-testing.py
@@ -84,31 +84,31 @@ def key_pressed(key):
   global walk_forward, walk_backward, knee_up, knee_down, hip_forward, hip_backward
   global knee_pos, hip_pos
   while True:
-  try: 
-    if key == "w":
-      kit.continuous_servo[wheel_loc].throttle=1
-    elif key == "s":
-      kit.continuous_servo[wheel_loc].throttle=-1
-    elif key == "q":
-      if knee_pos <= knee_max:
-        knee_pos += 5
-        kit.servo[knee_loc].angle = knee_pos
-    elif key == "e":
-      if knee_pos >= knee_min:
-        knee_pos -= 5
-        kit.servo[knee_loc].angle = knee_pos
-    elif key == "d":
-      if hip_pos <= hip_max:
-        hip_pos += 5
-        kit.servo[hip_loc].angle = hip_pos
-    elif key == "a":
-      if hip_pos >= hip_min:
-        hip_pos -= 5
-        kit.servo[hip_loc].angle = hip_pos
-  except AttributeError:
-    if key == "esc":
-      rover_shutdown()
-      return False
+    try: 
+      if key == "w":
+        kit.continuous_servo[wheel_loc].throttle=1
+      elif key == "s":
+        kit.continuous_servo[wheel_loc].throttle=-1
+      elif key == "q":
+        if knee_pos <= knee_max:
+          knee_pos += 5
+          kit.servo[knee_loc].angle = knee_pos
+      elif key == "e":
+        if knee_pos >= knee_min:
+          knee_pos -= 5
+          kit.servo[knee_loc].angle = knee_pos
+      elif key == "d":
+        if hip_pos <= hip_max:
+          hip_pos += 5
+          kit.servo[hip_loc].angle = hip_pos
+      elif key == "a":
+        if hip_pos >= hip_min:
+          hip_pos -= 5
+          kit.servo[hip_loc].angle = hip_pos
+    except AttributeError:
+      if key == "esc":
+        rover_shutdown()
+        return False
 
 def key_released(key):
   global walk_forward, walk_backward, knee_up, knee_down, hip_forward, hip_backward
