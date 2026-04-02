5c45f7f4d4eb22c8c4598d17688f9f954088bbe6
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-02 09:49:18 -0400

    created smooth-concurrent-rotation-testing.py
    created a testing script to try making smooth, concurrent motion for multiple servos. right now it is just smooth motion, concurrency will be added later.

---
+++ smooth-concurrent-rotation-testing.py
@@ -0,0 +1,25 @@
+from adafruit_servokit import ServoKit
+import time
+import asyncio
+
+kit = ServoKit(channels=16)
+kit.servo[0].angle = 
+async def rotate_servo(location, angle, period):
+  servo_pos_init = kit.servo[location].angle
+  servo_pos_fin = servo_pos_init + angle
+  time_init = time.time()
+  time_elapsed = time.time()-time_init
+  prog = time_elapsed/period
+  while True:
+    if prog >= 1:
+      break
+    servo_pos_curr = servo_pos_init + (servo_pos_fin-servo_pos_init)*prog
+    kit.servo[location].angle = servo_pos_curr
+    await asyncio.sleep(0.02)
+
+servo_inp_loc = int(input("Enter Servo Location: "))
+inp_angle = float(input("Enter Change in Angle: "))
+inp_period = float(input("Enter target period: "))
+
+rotate_servo(servo_inp_loc, inp_angle, inp_period)
+  
