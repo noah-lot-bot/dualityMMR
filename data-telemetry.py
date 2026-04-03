080f3adc4383789eddc66ed39ad1daf6385dac15
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-02 23:32:04 -0400

    created data-telemetry.py
    this script will house the data collection/communication tasks for sending imu/cpu data to the iotstack.

---
+++ data-telemetry.py
@@ -0,0 +1,6 @@
+import asyncio
+# static stability: RMS of pitch and roll
+# jerk: derivative of acceleration
+# vibration: fft of accel data
+# zero moment point control
+# pull voltage data? and cpu usage. should be easy enough
