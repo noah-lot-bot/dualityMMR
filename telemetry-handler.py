df61b8bfd8ffe81d13c2f63fb13d94b57171b125
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-04 18:34:09 -0400

    Update and rename data-telemetry.py to telemetry-handler.py
    rename and added in mqtt library

--- telemetry-handler.py
+++ telemetry-handler.py
@@ -1,6 +1,9 @@
 import asyncio
+import paho-mqtt
 # static stability: RMS of pitch and roll
 # jerk: derivative of acceleration
 # vibration: fft of accel data
 # zero moment point control
 # pull voltage data? and cpu usage. should be easy enough
+
+
