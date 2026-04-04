8263661dc9dfd0ea99f2112c00cc7574b204b7de
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-04 19:12:57 -0400

    Update telemetry-handler.py
    updated the telemetry handler so that it should publish to the local docker container, going to continue testing it

--- telemetry-handler.py
+++ telemetry-handler.py
@@ -6,19 +6,72 @@
 from paho-mqtt import client as mqtt
 import json
 import time
+import qwiic_ism330dhcx
 
 broker = "localhost"
 port = 1883
 topic = duality/telemetry/imu
 
+# create the client
 client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.Version2)
 
+# connect the client to the broker (Docker container)
 client.connect(broker,port,60)
 
+# start the messaging client
 client.loop_start()
 
-try:
-  while True:
+# read the imu data
+def imu_reader():
+  myIsm = qwiic_ism330dhcx.QwiicISM330DHCX()
+  myIsm.begin()
+  while myIsm.get_device_reset() == False:
+        time.sleep(1)
+    
+    time.sleep(0.100)
+
+    myIsm.set_device_config()
+    myIsm.set_block_data_update()
+ 
+    myIsm.set_accel_data_rate(myIsm.kXlOdr104Hz)
+    myIsm.set_accel_full_scale(myIsm.kXlFs4g)
+ 
+    myIsm.set_gyro_data_rate(myIsm.kGyroOdr104Hz)
+    myIsm.set_gyro_full_scale(myIsm.kGyroFs500dps)
+ 
+    myIsm.set_accel_filter_lp2()
+    myIsm.set_accel_slope_filter(myIsm.kLpOdrDiv100)
+ 
+    myIsm.set_gyro_filter_lp1()
+    myIsm.set_gyro_lp1_bandwidth(myIsm.kBwMedium)
+
+  try:
+    while True:
+      if myIsm.check_status():
+        accelData = myIsm.get_accel()
+        gyroData = myIsm.get_gyro()
+        time.sleep(0.100)
+        
+          imu_data = {
+            "x_accel": accelData.xData,
+            "y_accel": accelData.yData,
+            "z_accel": accelData.zData
+          }
+          gyro_data = {
+            "x_rot": gyroData.xData,
+            "y_rot": gyroData.yData,
+            "z_rot": gyroData.zData
+          }
+          payload = json.dumps({
+            imu_data_package: imu_data,
+            gyro_data_package: gyro_data
+          }
+          client.publish(topic, payload)
+          time.sleep(0.1)
+  except KeyboardInterrupt:
+    client.loop_stop()
+    client.disconnect()
+    
     
 
 
