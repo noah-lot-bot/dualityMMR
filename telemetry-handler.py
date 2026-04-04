4e9f87ba0131df3693b4078b170edad8de4c951b
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-04 18:50:59 -0400

    updated telemetry-handler.py
    created an mqtt client to handle telemetry data, going to import the basic imu reader script from sparkfun

--- telemetry-handler.py
+++ telemetry-handler.py
@@ -1,9 +1,25 @@
-import asyncio
-import paho-mqtt
 # static stability: RMS of pitch and roll
 # jerk: derivative of acceleration
 # vibration: fft of accel data
 # zero moment point control
 # pull voltage data? and cpu usage. should be easy enough
+from paho-mqtt import client as mqtt
+import json
+import time
+
+broker = "localhost"
+port = 1883
+topic = duality/telemetry/imu
+
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.Version2)
+
+client.connect(broker,port,60)
+
+client.loop_start()
+
+try:
+  while True:
+    
+
 
 
