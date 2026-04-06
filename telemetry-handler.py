30099e460e95b193287883b3949201ba4466dd28
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-05 23:40:19 -0400

    updated telemetry-handler.py
    going to separate data processing and collection from the messaging handler

--- telemetry-handler.py
+++ telemetry-handler.py
@@ -20,35 +20,6 @@ client.connect(broker,port,60)
 
 # start the messaging client
 client.loop_start()
-
-# read the imu data
-def imu_reader():
-  myIsm = qwiic_ism330dhcx.QwiicISM330DHCX()
-  myIsm.begin()
-  while myIsm.get_device_reset() == False:
-    time.sleep(1)
-
-    myIsm.set_device_config()
-    myIsm.set_block_data_update()
- 
-    myIsm.set_accel_data_rate(myIsm.kXlOdr104Hz)
-    myIsm.set_accel_full_scale(myIsm.kXlFs4g)
- 
-    myIsm.set_gyro_data_rate(myIsm.kGyroOdr104Hz)
-    myIsm.set_gyro_full_scale(myIsm.kGyroFs500dps)
- 
-    myIsm.set_accel_filter_lp2()
-    myIsm.set_accel_slope_filter(myIsm.kLpOdrDiv100)
- 
-    myIsm.set_gyro_filter_lp1()
-    myIsm.set_gyro_lp1_bandwidth(myIsm.kBwMedium)
-
-  if myIsm.check_status():
-    accelData = myIsm.get_accel()
-    gyroData = myIsm.get_gyro()
-    time.sleep(0.100)
-    return accelData, gyroData
-  return None, None
   
 try:
   while True:
