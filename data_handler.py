70056ad2b8418754a561e023bd9cb98087c410aa
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-06 13:49:09 -0400

    Update data_handler.py
    HELP ME

--- data_handler.py
+++ data_handler.py
@@ -16,25 +16,17 @@ def imu_reader():
 	myIsm.device_reset()
 
 	while myIsm.get_device_reset() == False:
-	time.sleep(1)
-		
-	time.sleep(0.100)
-
-	myIsm.set_device_config()
-	myIsm.set_block_data_update()
-
-	myIsm.set_accel_data_rate(myIsm.kXlOdr104Hz)
-	myIsm.set_accel_full_scale(myIsm.kXlFs4g)
-
-	myIsm.set_gyro_data_rate(myIsm.kGyroOdr104Hz)
-	myIsm.set_gyro_full_scale(myIsm.kGyroFs500dps)
-
-	myIsm.set_accel_filter_lp2()
-	myIsm.set_accel_slope_filter(myIsm.kLpOdrDiv100)
-
-	myIsm.set_gyro_filter_lp1()
-	myIsm.set_gyro_lp1_bandwidth(myIsm.kBwMedium)
-
+		time.sleep(1)
+		myIsm.set_device_config()
+		myIsm.set_block_data_update()
+		myIsm.set_accel_data_rate(myIsm.kXlOdr104Hz)
+		myIsm.set_accel_full_scale(myIsm.kXlFs4g)
+		myIsm.set_gyro_data_rate(myIsm.kGyroOdr104Hz)
+		myIsm.set_gyro_full_scale(myIsm.kGyroFs500dps)
+		myIsm.set_accel_filter_lp2()
+		myIsm.set_accel_slope_filter(myIsm.kLpOdrDiv100)
+		myIsm.set_gyro_filter_lp1()
+		myIsm.set_gyro_lp1_bandwidth(myIsm.kBwMedium)
 	while True:
 		if myIsm.check_status():
 			accelData = myIsm.get_accel()
