f35b1e58e97093787e6002d5fd897fd95ea7d6f2
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-08 15:25:42 -0400

    Update gait_testing.py
    bug fixes

--- gait_testing.py
+++ gait_testing.py
@@ -2,7 +2,7 @@ import asyncio
 from sshkeyboard import listen_keyboard, stop_listening
 from gait_definitions import (flat_ground_gait, turn_right_gait, turn_left_gait, set_neutral, front_left_leg, front_right_leg, back_left_leg, back_right_leg, roll_forward_gait) 
 
-set_neutral()
+set_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
 
 async def key_pressed(key):
   try: 
