2fbf2d257efe9415795660fb31023c81c4c5c2dc
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-08 15:55:16 -0400

    Update gait_definitions.py
    bug fixes

--- gait_definitions.py
+++ gait_definitions.py
@@ -20,7 +20,7 @@ class leg:
     self.knee_neutral = knee_neutral
 
 # we are going to hard code the servo locations because its way easier than asking for them (WIP)
-front_left_leg = leg(13,14,12, 0,95,21, 60,130,85)
+front_left_leg = leg(13,14,12, 0,95,21, 59,125,84)
 front_right_leg = leg(2,1,0, 40,131,113, 20,90,55)
 back_left_leg = leg(11,10,9, 30,120,105, 70,133,107)
 back_right_leg = leg(4,5,6, 28,127,48, 60,135,92)
