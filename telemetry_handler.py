aa407ab7d53a82e4d9a6251ae09486409feeeba8
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 11:07:25 -0400

    added asyncio
    hopefully implemented asyncio so this can be called in the motion code

--- telemetry_handler.py
+++ telemetry_handler.py
@@ -6,6 +6,7 @@
 import paho.mqtt.client as mqtt
 import json
 import time
+import asyncio
 from data_handler import imu_reader
 
 broker = "192.168.8.157"
@@ -20,23 +21,24 @@ client.connect(broker,port,60)
 client.loop_start()
 
 # non blocking data collection
-try:
-  while True:
-    accelData, gyroData = imu_reader()
-    imu_data = {
+async def telemetry_handler():
+  try:
+    while True:
+      accelData, gyroData = imu_reader()
+      imu_data = {
         "x_accel": accelData.xData,
         "y_accel": accelData.yData,
         "z_accel": accelData.zData,
         "x_rot": gyroData.xData,
         "y_rot": gyroData.yData,
         "z_rot": gyroData.zData
-    }
-    payload = json.dumps(imu_data)
-    client.publish(topic, payload)
-    time.sleep(0.1)
-finally:
-  client.loop_stop()
-  client.disconnect()
+      }
+      payload = json.dumps(imu_data)
+      client.publish(topic, payload)
+      time.sleep(0.1)
+  finally:
+    client.loop_stop()
+    client.disconnect()
     
     
 
