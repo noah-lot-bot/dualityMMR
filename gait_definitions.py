84824c418763f971bd0673a91862517986a96fce
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 14:01:43 -0400

    Update gait_definitions.py
    bug fixes

--- gait_definitions.py
+++ gait_definitions.py
@@ -101,117 +101,69 @@ async def wait_for_user():
 async def upslope_gait(front_left_leg, front_right_leg, back_left_leg, back_right_leg):
   # back left crawl
   await set_wheel_speed(back_right_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(back_right_leg.hip_location, back_right_leg.hip_max-back_right_leg.hip_neutral, 3)
-  await wait_for_user()
   await set_wheel_speed(back_right_leg.wheel_location, 0.1)
-  await wait_for_user()
   
   await rotate_servo(back_left_leg.knee_location, back_left_leg.knee_neutral-back_left_leg.knee_max, 2)
-  await wait_for_user()
   await rotate_servo(back_left_leg.hip_location, back_left_leg.hip_max-back_left_leg.hip_neutral, 2)
-  await wait_for_user()
   await rotate_servo(back_left_leg.knee_location, back_left_leg.knee_max-back_left_leg.knee_neutral, 2)
-  await wait_for_user()
   
   await set_wheel_speed(back_left_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(back_left_leg.hip_location, back_left_leg.hip_neutral-back_left_leg.hip_max, 2)
-  await wait_for_user()
   await set_wheel_speed(back_left_leg.wheel_location, 0.1)
-  await wait_for_user()
   
   await set_wheel_speed(back_right_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(back_right_leg.hip_location, back_right_leg.hip_neutral-back_right_leg.hip_max, 3)
-  await wait_for_user()
   await set_wheel_speed(back_right_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   # front left crawl
   await set_wheel_speed(front_right_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_min-front_right_leg.hip_neutral, 3) 
-  await wait_for_user()
   await set_wheel_speed(front_right_leg.wheel_location, 0.1)
-  await wait_for_user()
   
   await rotate_servo(front_left_leg.knee_location, front_left_leg.knee_max-front_left_leg.knee_neutral, 2)
-  await wait_for_user()
   await rotate_servo(front_left_leg.hip_location, front_left_leg.hip_max-front_left_leg.hip_neutral, 2)
-  await wait_for_user()
   await rotate_servo(front_left_leg.knee_location, front_left_leg.knee_neutral-front_left_leg.knee_max, 2)
-  await wait_for_user()
   
   await set_wheel_speed(front_left_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(front_left_leg.hip_location, front_left_leg.hip_neutral-front_left_leg.hip_max, 2)
-  await wait_for_user()
   await set_wheel_speed(front_left_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   await set_wheel_speed(front_right_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_neutral-front_right_leg.hip_min, 3)
-  await wait_for_user()
   await set_wheel_speed(front_right_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   # back right crawl
   await set_wheel_speed(back_left_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(back_left_leg.hip_location, back_left_leg.hip_min-back_left_leg.hip_neutral, 3) 
-  await wait_for_user()
   await set_wheel_speed(back_left_leg.wheel_location, 0.1)
-  await wait_for_user()
   
   await rotate_servo(back_right_leg.knee_location, back_right_leg.knee_max-back_right_leg.knee_neutral, 2) #too far
-  await wait_for_user()
   await rotate_servo(back_right_leg.hip_location, back_right_leg.hip_min-back_right_leg.hip_neutral, 2) #!!! too far
-  await wait_for_user()
   await rotate_servo(back_right_leg.knee_location, back_right_leg.knee_neutral-back_right_leg.knee_max, 2)
-  await wait_for_user()
   await set_wheel_speed(back_right_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(back_right_leg.hip_location, back_right_leg.hip_neutral-back_right_leg.hip_min, 2)
-  await wait_for_user()
   await set_wheel_speed(back_right_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   await set_wheel_speed(back_left_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(back_left_leg.hip_location, back_left_leg.hip_neutral-back_left_leg.hip_min, 3)
-  await wait_for_user()
   await set_wheel_speed(back_left_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   # front right crawl
   await set_wheel_speed(front_left_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(front_left_leg.hip_location, front_right_leg.hip_neutral-front_right_leg.hip_min, 3)
-  await wait_for_user()
   await set_wheel_speed(front_left_leg.wheel_location, 0.1)
-  await wait_for_user()
   
   await rotate_servo(front_right_leg.knee_location, front_right_leg.knee_min-front_right_leg.knee_neutral, 2) #!!!
-  await wait_for_user()
   await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_min-front_right_leg.hip_neutral, 2)
-  await wait_for_user()
   await rotate_servo(front_right_leg.knee_location, front_right_leg.knee_neutral-front_right_leg.knee_min, 2)
-  await wait_for_user()
   await set_wheel_speed(front_right_leg.wheel_location, 0.5)
-  await wait_for_user()
   await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_neutral-front_right_leg.hip_min, 2) 
-  await wait_for_user()
   await set_wheel_speed(front_right_leg.wheel_location, 0.1)
-  await wait_for_user()
 
   await set_wheel_speed(front_left_leg.wheel_location, -0.5)
-  await wait_for_user()
   await rotate_servo(front_left_leg.hip_location, front_left_leg.hip_neutral-front_left_leg.hip_max, 3)
-  await wait_for_user()
   await set_wheel_speed(front_left_leg.wheel_location, 0.1)
-  await wait_for_user()
 
 async def turn_right_gait(front_left_leg, front_right_leg, back_left_leg, back_right_leg):
   await asyncio.gather(
