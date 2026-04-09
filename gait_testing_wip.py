adf2925c95229b375d6543ec8563d3f9fdb29212
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 15:35:56 -0400

    updated
    changed to turtle gait

--- gait_testing_wip.py
+++ gait_testing_wip.py
@@ -1,11 +1,11 @@
 import asyncio 
 from sshkeyboard import listen_keyboard_manual, stop_listening
-from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, front_left_leg, front_right_leg, back_left_leg, back_right_leg, roll_forward_gait, roll_backward_gait, raise_chassis_gait, lower_chassis_gait, upslope_gait) 
+from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, front_left_leg, front_right_leg, back_left_leg, back_right_leg, roll_forward_gait, roll_backward_gait, raise_chassis_gait, lower_chassis_gait, upslope_gait, turtle_gait) 
 from telemetry_handler import telemetry_handler
 
 async def main():
   await set_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
-  await upslope_gait(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
+  await turtle_gait(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
   
 if __name__=="__main__":
   try:
