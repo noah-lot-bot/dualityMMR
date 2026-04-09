703c2d7f4918eb0b1ed23badbd23183a9bd2be1a
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 13:46:05 -0400

    Update gait_definitions.py
    bug fixes

--- gait_definitions.py
+++ gait_definitions.py
@@ -131,7 +131,7 @@ async def upslope_gait(front_left_leg, front_right_leg, back_left_leg, back_righ
   # front left crawl
   await set_wheel_speed(front_right_leg.wheel_location, -0.5)
   await wait_for_user()
-  await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_min-front_right_leg.hip_neutral, 3) #!!! wrong way
+  await rotate_servo(front_right_leg.hip_location, front_right_leg.hip_min-front_right_leg.hip_neutral, 3) 
   await wait_for_user()
   await set_wheel_speed(front_right_leg.wheel_location, 0.1)
   await wait_for_user()
@@ -145,7 +145,7 @@ async def upslope_gait(front_left_leg, front_right_leg, back_left_leg, back_righ
   
   await set_wheel_speed(front_left_leg.wheel_location, -0.5)
   await wait_for_user()
-  await rotate_servo(front_left_leg.hip_location, front_left_leg.hip_neutral-front_left_leg.hip_min, 2)
+  await rotate_servo(front_left_leg.hip_location, front_left_leg.hip_neutral-front_left_leg.hip_max, 2)
   await wait_for_user()
   await set_wheel_speed(front_left_leg.wheel_location, 0.1)
   await wait_for_user()
