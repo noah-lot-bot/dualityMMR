6d877c0a154b656bb02c9c4251fcdc41c6a3bfc5
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 11:53:34 -0400

    Update gait_testing.py
    listen keyboard manual

--- gait_testing.py
+++ gait_testing.py
@@ -1,5 +1,5 @@
 import asyncio 
-from sshkeyboard import listen_keyboard, stop_listening
+from sshkeyboard import listen_keyboard_manual, stop_listening
 from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, front_left_leg, front_right_leg, back_left_leg, back_right_leg, roll_forward_gait, roll_backward_gait, raise_chassis_gait, lower_chassis_gait) 
 from telemetry_handler import telemetry_handler
 
