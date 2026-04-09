a535bf28abb8de31c7922798c4b7c4afc404a972
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 01:18:24 -0400

    fixed broker
    added broker as fixed ip of the pi

--- telemetry_handler.py
+++ telemetry_handler.py
@@ -8,7 +8,7 @@ import json
 import time
 from data_handler import imu_reader
 
-broker = string(input("Input the broker address (IP) of the Pi: "))
+broker = "192.168.8.157"
 port = 1883
 topic = "duality/telemetry"
 
