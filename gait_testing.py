c955b380d06f61a45424b91f82ae4b365a509b04
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 11:47:00 -0400

    Update gait_testing.py
    removed data collector

--- gait_testing.py
+++ gait_testing.py
@@ -4,7 +4,6 @@ from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, fron
 from telemetry_handler import telemetry_handler
 
 asyncio.run(set_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg))
-asyncio.run(telemetry_handler())
 
 async def key_pressed(key):
   try: 
