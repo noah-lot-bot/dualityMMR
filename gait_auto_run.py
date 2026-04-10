db1c9d1f8fbac26f6c2b878012b632f13971cc46
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-10 00:50:59 -0400

    Update gait_auto_run.py
    bug fixes

--- gait_auto_run.py
+++ gait_auto_run.py
@@ -4,7 +4,7 @@ from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, fron
 from telemetry_handler import telemetry_handler
 
 async def main():
-  await set_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
+  await initialize_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
   while True:
     await turtle_gait(front_left_leg, front_right_leg, back_left_leg, back_right_leg)
   
