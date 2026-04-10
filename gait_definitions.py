869b7320b83f96ba5eff10e16d4c31e6d9c95089
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-09 22:24:48 -0400

    changed absolute rotation
    trying to implement absolute servo rotation with motion smoothing for the purposes of easier gait design

--- gait_definitions.py
+++ gait_definitions.py
@@ -63,8 +63,20 @@ async def rotate_servo(location, angle, period):
     kit.servo[location].angle = servo_pos_curr
     await asyncio.sleep(0.02)
 
-async def rotate_servo_absolute(location, angle):
- kit.servo[location].angle = angle
+async def rotate_servo_absolute(location, angle, period):
+  servo_pos_init = kit.servo[location].angle
+  servo_pos_fin = angle
+  time_init = time.time()
+  while True:
+    time_elapsed = time.time()-time_init
+    progress = time_elapsed/period
+    s_curve = (1-math.cos(progress*math.pi))/2
+    if progress >= 1:
+      break
+    servo_pos_curr = servo_pos_init + (servo_pos_fin-servo_pos_init)*s_curve
+    #servo_pos_curr = await movement_restrictor(location, servo_pos_init, servo_pos_curr)
+    kit.servo[location].angle = servo_pos_curr
+    await asyncio.sleep(0.02)
 
 # awaitable wheel speed set
 async def set_wheel_speed(wheel_location, speed):
