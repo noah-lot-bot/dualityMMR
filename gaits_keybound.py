37cad4622d90af303a2155d8be1f615d72e44952
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-10 00:39:25 -0400

    set neutral and initialize neutral
    added the functions

--- gaits_keybound.py
+++ gaits_keybound.py
@@ -1,9 +1,9 @@
 import asyncio 
 from sshkeyboard import listen_keyboard_manual, stop_listening
-from gait_definitions import (turn_right_gait, turn_left_gait, set_neutral, front_left_leg, front_right_leg, back_left_leg, back_right_leg, roll_forward_gait, roll_backward_gait, raise_chassis_gait, lower_chassis_gait, upslope_gait) 
+import gait_definitions
 from telemetry_handler import telemetry_handler
 
-asyncio.run(set_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg))
+asyncio.run(initialize_neutral(front_left_leg, front_right_leg, back_left_leg, back_right_leg))
 
 async def key_pressed(key):
   try: 
