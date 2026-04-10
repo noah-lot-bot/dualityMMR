a2fbbc25b15c953278ce354c19ad4ea8023a7391
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-10 00:48:13 -0400

    Update gait_definitions.py
    bug fixes

--- gait_definitions.py
+++ gait_definitions.py
@@ -201,8 +201,8 @@ async def turtle_gait(front_left_leg, front_right_leg, back_left_leg, back_right
   await asyncio.gather(
     rotate_servo_absolute(front_left_leg.hip_location, front_left_leg.hip_min, 2),
     rotate_servo_absolute(front_right_leg.hip_location, front_right_leg.hip_max, 2),
-    rotate_servo_absolute(back_right_leg.hip_location, back_right_leg.hip_min, 2),
-    rotate_servo_absolute(back_left_leg.hip_location, back_left_leg.hip_max, 2)
+    rotate_servo_absolute(back_right_leg.hip_location, back_right_leg.hip_max, 2),
+    rotate_servo_absolute(back_left_leg.hip_location, back_left_leg.hip_min, 2)
   )
   # lower body
   await asyncio.gather(
