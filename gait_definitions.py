0771271e8736079729f238de125ea88f8c4ecba0
Noah R. <noahemmanuelramirez@gmail.com>
2026-04-23 15:25:47 -0400

    reverse turtle gait fix
    wheel spin other way and reflected

--- gait_definitions.py
+++ gait_definitions.py
@@ -180,6 +180,50 @@ async def turtle_gait(front_left_leg, front_right_leg, back_left_leg, back_right
   )
 
 async def turtle_gait_fix(front_left_leg, front_right_leg, back_left_leg, back_right_leg):
+  # move hips backward from neutral
+  await asyncio.gather(
+    rotate_servo_absolute(front_left_leg.hip_location, front_left_leg.hip_min, 3),
+    rotate_servo_absolute(front_right_leg.hip_location, front_right_leg.hip_max, 3),
+    rotate_servo_absolute(back_right_leg.hip_location, 60, 3),
+    rotate_servo_absolute(back_left_leg.hip_location, 120, 3)
+  )
+  # lower body
+  await asyncio.gather(
+    rotate_servo_absolute(back_left_leg.knee_location, back_left_leg.knee_min, 2),
+    rotate_servo_absolute(front_left_leg.knee_location, front_left_leg.knee_max, 2),
+    rotate_servo_absolute(back_right_leg.knee_location, back_right_leg.knee_max, 2),
+    rotate_servo_absolute(front_right_leg.knee_location, front_right_leg.knee_min, 2)
+  )
+  # roll wheels backwards
+  await asyncio.gather(
+    set_wheel_speed(front_left_leg.wheel_location, 0.5),
+    set_wheel_speed(back_left_leg.wheel_location, 0.5),
+    set_wheel_speed(front_right_leg.wheel_location, -0.5),
+    set_wheel_speed(back_right_leg.wheel_location, -0.5)
+  )
+    # move hips forward
+  await asyncio.gather(
+    rotate_servo_absolute(front_left_leg.hip_location, 95, 2),
+    rotate_servo_absolute(front_right_leg.hip_location, 50, 2),
+    rotate_servo_absolute(back_right_leg.hip_location, back_right_leg.hip_min, 2),
+    rotate_servo_absolute(back_left_leg.hip_location, back_left_leg.hip_max, 2)
+  )
+  # stop wheels
+  await asyncio.gather(
+    set_wheel_speed(front_left_leg.wheel_location, 0.1),
+    set_wheel_speed(back_left_leg.wheel_location, 0.1),
+    set_wheel_speed(front_right_leg.wheel_location, 0.1),
+    set_wheel_speed(back_right_leg.wheel_location, 0.1)
+  )
+  # raise body
+  await asyncio.gather(
+    rotate_servo_absolute(back_left_leg.knee_location, back_left_leg.knee_max-10, 2),
+    rotate_servo_absolute(front_left_leg.knee_location, front_left_leg.knee_min+10, 2),
+    rotate_servo_absolute(back_right_leg.knee_location, back_right_leg.knee_min+10, 2),
+    rotate_servo_absolute(front_right_leg.knee_location, front_right_leg.knee_max-10, 2)
+  )
+
+async def reverse_turtle_gait_fix(front_left_leg, front_right_leg, back_left_leg, back_right_leg):
   # move hips forward
   await asyncio.gather(
     rotate_servo_absolute(front_left_leg.hip_location, 95, 2),
@@ -196,10 +240,10 @@ async def turtle_gait_fix(front_left_leg, front_right_leg, back_left_leg, back_r
   )
   # roll wheels backwards
   await asyncio.gather(
-    set_wheel_speed(front_left_leg.wheel_location, 0.5),
-    set_wheel_speed(back_left_leg.wheel_location, 0.5),
-    set_wheel_speed(front_right_leg.wheel_location, -0.5),
-    set_wheel_speed(back_right_leg.wheel_location, -0.5)
+    set_wheel_speed(front_left_leg.wheel_location, -0.5),
+    set_wheel_speed(back_left_leg.wheel_location, -0.5),
+    set_wheel_speed(front_right_leg.wheel_location, 0.5),
+    set_wheel_speed(back_right_leg.wheel_location, 0.5)
   )
   # move hips backward from neutral
   await asyncio.gather(
